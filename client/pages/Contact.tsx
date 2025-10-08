bfb5839e295d151c9e9909bbed8712b01b459163
azorixsemivlsitraining <azorixsemi.vlsi.training@gmail.com>
2025-10-08 11:47:44 +0530

    Update Contact.tsx

--- client/pages/Contact.tsx
+++ client/pages/Contact.tsx
@@ -551,7 +551,7 @@ export default function Contact() {
             <div className="bg-white rounded-lg p-6 shadow-sm">
               {/* Interactive Google Map */}
               <div className="mb-6">
-                <iframe
+                {/* <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1902.654284473667!2d78.34529508884089!3d17.4927775458531!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb9267acba612f%3A0xa3caf6980786c818!2z4KSu4KWI4KSk4KWN4KSw4KWAIOCkqOCkl-CksCwg4KSu4KSm4KS_4KSo4KS-4KSX4KWB4KSm4KS-LCDgpLngpYjgpKbgpLDgpL7gpKzgpL7gpKYsIOCkpOClh-CksuCkguCkl-CkvuCkqOCkviA1MDAwNDk!5e0!3m2!1shi!2sin!4v1759903743994!5m2!1shi!2sin"
                   width="100%"
                   height="400"
@@ -561,7 +561,7 @@ export default function Contact() {
                   referrerPolicy="no-referrer-when-downgrade"
                   className="rounded-lg"
                   title="Azorix VLSI Location - Jubilee Enclave, HITEC City"
-                ></iframe>
+                ></iframe> */}
               </div>
 
               {/* Location Details */}
@@ -575,14 +575,14 @@ export default function Contact() {
                   
                   Hyderabad, Telangana 500049, India
                 </p>
-                <a
+                {/* <a
                   href="https://maps.app.goo.gl/Y8y8PqK97gkQiNJH9"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="inline-flex items-center text-vlsi-600 hover:text-vlsi-700 font-medium"
                 >
                   📍 Open in Google Maps
-                </a>
+                </a> */}
               </div>
 
               {/* Transportation Options */}
